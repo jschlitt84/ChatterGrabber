426c9f99b3b485203aa455d98dcd190d31e8ee6e
James Schlitt <jslongboards@gmail.com>
2014-08-18 16:53:28 -0400

    typo

--- libraries/MergeCaches.py
+++ libraries/MergeCaches.py
@@ -3,9 +3,10 @@ import KwikECache as kwik
 import sys, os
 
 data = dict()
+print sys.argv[1:]
 pickleIn1 = open(sys.argv[1], "rb")
 pickleLoaded1 = cPickle.load(pickleIn1)
 pickleIn2 = open(sys.argv[2], "rb")
 pickleLoaded2 = cPickle.load(pickleIn2)
 pickleLoaded1.update(pickleLoaded2)
-kwik.updateCache(pickleLoaded1,sys.argv[3],25):
+kwik.updateCache(pickleLoaded1,sys.argv[3],25)
