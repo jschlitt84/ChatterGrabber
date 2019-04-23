a11364280c7850da31fbcc12d7852c6d329612e7
James Schlitt <jslongboards@gmail.com>
2019-04-23 16:28:12 -0400

    Remove debug, extend timeout

--- libraries/GISpy.py
+++ libraries/GISpy.py
@@ -534,8 +534,7 @@ def sendCSV(cfg, directory,extra = ''):
             'attachment; filename="%s"' % os.path.basename(attachment))
     msg.attach(part)
 
-    print cfg['GDI']
-    mailServer = smtplib.SMTP("smtp.gmail.com", 587, None, 30)
+    mailServer = smtplib.SMTP("smtp.gmail.com", 587, None, 120)
     mailServer.ehlo()
     mailServer.starttls()
     mailServer.ehlo()
